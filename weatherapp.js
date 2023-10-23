a21dc13266114dba10bf6476f502f1564bb1d7ae
Suraj Singh <kumarniteshoffice786@gmail.com>
2023-10-23 15:09:46 +0530

    api issue fixed

--- weatherapp.js
+++ weatherapp.js
@@ -1,51 +1,45 @@
 const unirest = require("unirest");
 const express = require('express');
-const bodyText = require('body-parser')
+const bodyParser = require('body-parser');
 
 const app = express();
-app.use(bodyText.urlencoded({extended:true})); //To use body parser with post request
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
 
-//include all static files so we can use CSS
-app.use(express.static( __dirname + '/public'));
-
-
-//main page
-app.get("/", function(req,res) {
-	res.sendFile(__dirname +"/public/index.html" );
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/public/index.html");
 });
 
-// Display the information when there is post request
-app.post("/", function(request,response) {
+app.post("/", (request, response) => {
+  const city = request.body.city;
+  
+  console.log({city})
 
-  //Get the weather data
-  const req = unirest("GET", "https://community-open-weather-map.p.rapidapi.com/weather");
+  if (!city) {
+    return response.status(400).json({ error: 'City is missing in the request body' });
+  }
 
-  let city = request.body.city;
-  // city = city + ", USA"  //Add country if needed
-
+  const req = unirest("GET", `https://open-weather13.p.rapidapi.com/city/${city}`);
   req.query({
     "q": city,
     "lang": "en",
     "units": "imperial"
   });
 
-// Update your API keys
   req.headers({
-    "x-rapidapi-key": "b0ecde0aa7msh4000c55386b3d0ap1a619djsn5b8ac1b1b077",
-    "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+    "X-RapidAPI-Key": "3ff511dc1amsh2077b883c443528p124aa7jsn7c57d1e3f041",
+    "X-RapidAPI-Host": "open-weather13.p.rapidapi.com",
     "useQueryString": true
   });
 
-
   req.end(function (res) {
     if (res.error) throw new Error(res.error);
-    response.send(res.body); //Update
+    response.send(res.body);
   });
-
-
 });
 
 let port = process.env.PORT || 8002;
 app.listen(port, function() {
-    console.log ("Server running on port 8002");
-})
+    console.log("Server running on port " + port);
+});
